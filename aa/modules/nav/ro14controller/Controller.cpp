6e77ecc51fb8719c85adbd20ce22f0f95dbdfb73
Fritz Ulbrich <fritz.ulbrich@fu-berlin.de>
2014-11-18 11:34:02 +0100

    controller for excercise 5

---
+++ aa/modules/nav/ro14controller/Controller.cpp
@@ -0,0 +1,220 @@
+#include "Controller.h"
+
+#include <util/TaskContextFactory.h>
+#include <util/OrocosHelperFunctions.h>
+#include <math/AutoMath.h>
+#include <math/PathSpline.h>
+
+
+namespace aa
+{
+namespace modules
+{
+namespace nav
+{
+namespace ro14controller
+{
+
+REGISTERTASKCONTEXT(Controller);
+
+
+using namespace std;
+using namespace RTT;
+using namespace boost;
+using namespace ::math;
+using namespace ::util;
+using RTT::Logger;
+
+
+
+Controller::Controller(string const & name)
+	: ::util::RtTaskContext(name)
+
+	// read ports
+	, mPlanIn("PlanIn")
+	, mEgoStateIn("EgoStateIn")
+
+	// write ports
+	, mSteerOut("SteerOut")
+	, mSpeedOut("SpeedOut")
+	, mGearOut("GearOut")
+	, mControllerDataOut("ControllerDataOut")
+
+	// general properties
+    , mWantedSpeed(100 * KMH_2_MS)
+    , mConstantThrottle(0.25)
+    , mConstantSteer(0.0)
+
+    //attributes
+    , mCounter(0)
+{
+	ports()->addPort(mPlanIn);
+	ports()->addPort(mEgoStateIn);
+
+	ports()->addPort(mSteerOut);
+	ports()->addPort(mSpeedOut);
+	ports()->addPort(mGearOut);
+	ports()->addPort(mControllerDataOut);
+
+    addProperty("WantedSpeed", mWantedSpeed).doc("wanted speed in m/s");
+    addProperty("ConstantThrottle", mConstantThrottle).doc("wanted constant throttle to be maintained");
+    addProperty("ConstantSteer", mConstantSteer).doc("constant steer value");
+
+    addAttribute("Counter", mCounter);
+
+}
+
+
+Controller::~Controller()
+{}
+
+
+bool Controller::startHook()
+{
+	Logger::In in("Controller");
+
+	REQUIRED_PORT(mPlanIn);
+	REQUIRED_PORT(mEgoStateIn);
+
+	REQUIRED_PORT(mSteerOut);
+	REQUIRED_PORT(mSpeedOut);
+    REQUIRED_PORT(mGearOut);
+	OPTIONAL_PORT(mControllerDataOut);
+
+    mStartTime.stamp();
+
+    string filename = "controller.csv";
+    mOutputStream.open(filename.c_str());
+
+    return true;
+}
+
+void Controller::updateHook()
+{
+	Logger::In in("Controller");
+
+
+    ///read data
+    // read egostate
+	mEgoStateIn.read(mCurEgoState);
+
+	if (mCurEgoState == TimeStamp()) {
+        Logger::log() << Logger::Warning << "Invalid or no EgoState";
+        return;
+	}
+
+    // read plan
+    mPlanIn.read(mCurPlan);
+
+    if (!mCurPlan) {
+        Logger::log() << Logger::Warning << "Got no plan";
+        return;
+    }
+
+
+    //update counter
+    mCounter++;
+
+
+    //debug messages
+    Logger::log() << Logger::Debug << "=============================" << Logger::endl;
+
+    now.stamp();
+    flt timeSinceStart = 1E-9f * RTT::os::TimeService::ticks2nsecs(now - mStartTime);
+    Logger::log() << Logger::Debug << "time since start (s):" << timeSinceStart << Logger::endl;
+
+    flt curSpeed = mCurEgoState.vehicleSpeed();
+    Logger::log() << Logger::Debug << "car speed:" << curSpeed << Logger::endl;
+
+    Vec3 curPosition = mCurEgoState.position();
+    Logger::log() << Logger::Debug << "car position:" << curPosition.transpose() << Logger::endl;
+
+    Vec3 curOrientation = mCurEgoState.forwardDirection();
+    Logger::log() << Logger::Debug << "car orientation:" << curOrientation.transpose() << Logger::endl;
+
+
+    flt closestSqrDistToPlan;
+    flt closestParamOnPlan;
+    std::pair<flt, flt> const dom((*mCurPlan).domain());
+    tie(closestSqrDistToPlan, closestParamOnPlan) = findClosestPoint(*mCurPlan, dom.first, dom.second, (dom.first + dom.second) / 2.0, mCurEgoState.position());
+    Logger::log() << Logger::Debug << "dist to closest point on plan:" << sqrt(closestSqrDistToPlan) << Logger::endl;
+    Logger::log() << Logger::Debug << "param of closest point on plan:" << closestParamOnPlan << Logger::endl;
+
+    Vec3 closestPointOnPlan = (*mCurPlan)(closestParamOnPlan);
+    Logger::log() << Logger::Debug << "closest point on plan:" << closestPointOnPlan.transpose() << Logger::endl;
+
+    Vec3 closestPointOnPlanDir = mCurPlan->firstDerivative(closestParamOnPlan);
+    Logger::log() << Logger::Debug << "closest point direction:" << closestPointOnPlanDir.transpose() << Logger::endl;
+
+
+    //calculate throttle and steering values
+    TimedFlt speedCorrection;
+    speedCorrection.data = getThrottleBrakePosition(mCurControllerData.curSpeed, mWantedSpeed);
+    speedCorrection.stamp();
+
+    flt steerCorrection;
+    steerCorrection = getSteeringPosition(mCurPlan);
+
+
+    //create ControllerData for display module
+    mCurControllerData = aa::modules::nav::controller::data::TimedControllerData(now, aa::modules::nav::controller::data::ControllerData(mCurEgoState, ::modules::models::carstate::TimedCarState(), mCurPlan, closestParamOnPlan, closestSqrDistToPlan, NAN, NAN, NAN, NAN, NAN, ""));
+    mCurControllerData.wantedSpeed = mWantedSpeed;
+    mCurControllerData.speedCorrection = speedCorrection.data;
+    mCurControllerData.steerCorrection = steerCorrection;
+    mCurControllerData.stamp();
+
+
+    ///write to OutputPorts
+    // write out steer and speed correction
+    mSpeedOut.write(speedCorrection);
+    mSteerOut.write(steerCorrection);
+
+    //set gear to drive
+    mGearOut.write(8);
+
+    // write out controller data
+	mControllerDataOut.write(mCurControllerData);
+
+
+    ///write to file
+    mOutputStream << "time: " << timeSinceStart << std::endl;
+
+
+}
+
+void Controller::stopHook()
+{
+    Logger::In in("Controller");
+
+    mOutputStream.close();
+}
+
+flt Controller::getThrottleBrakePosition(flt curSpeed, flt wantedSpeed)
+{
+	Logger::In in("Controller");
+
+    //insert your code here
+
+    return mConstantThrottle;
+}
+
+
+
+flt Controller::getSteeringPosition(aa::modules::nav::controller::Plan_ptr plan)
+{
+	Logger::In in("Controller");
+
+    //insert your code here
+
+    return mConstantSteer;
+}
+
+
+
+
+
+
+}
+}
+}
+}
---
+++ aa/modules/nav/ro14controller/Controller.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <util/RtTaskContext.h>
+#include <util/Ports.h>
+#include <core/TimedData.h>
+#include <iostream>
+#include <deque>
+
+#include <aa/modules/nav/controller/Plan.h>
+#include <aa/modules/nav/controller/data/ControllerData.h>
+
+#include <modules/models/egostate/EgoState.h>
+
+
+namespace aa
+{
+namespace modules
+{
+namespace nav
+{
+namespace ro14controller
+{
+
+class Controller
+	: public util::RtTaskContext
+{
+public:
+	typedef ::math::flt flt;
+    typedef TimedData< PlainDataHolder< ::math::flt > > TimedFlt;
+	typedef ::math::Vec3 Vec3;
+
+    explicit Controller(std::string const & name);
+    virtual ~Controller();
+
+	virtual bool startHook();
+	virtual void updateHook();
+	virtual void stopHook();
+
+private:
+
+	/** @name OutputPorts: */
+	//@{
+	RTT::OutputPort< flt > mSteerOut;
+	RTT::OutputPort< TimedFlt > mSpeedOut;
+	RTT::OutputPort< int > mGearOut;
+    RTT::OutputPort< aa::modules::nav::controller::data::TimedControllerData > mControllerDataOut;
+	//@}
+
+	/** @name InputPorts: */
+	//@{
+    RTT::InputPort< aa::modules::nav::controller::Plan_ptr > mPlanIn;
+	RTT::InputPort< TimedEgoState > mEgoStateIn;
+    //@}
+
+	/** @name Attributes: general attributes */
+    int mCounter;
+
+    /** @name Properties */
+    flt mWantedSpeed;
+    flt mConstantThrottle;
+    flt mConstantSteer;
+
+	/** members */
+    aa::modules::nav::controller::data::TimedControllerData mCurControllerData;
+    aa::modules::nav::controller::Plan_ptr mCurPlan;
+	TimedEgoState mCurEgoState;
+
+	TimeStamp now;
+    TimeStamp mStartTime;
+
+    std::ofstream mOutputStream;
+
+
+	/** calculate a throttle position given current and wanted speed (uses either linear, pid controller)
+	* @param curSpeed current speed of the car in m/s
+	* @param wantedSpeed desired speed of the car in m/s
+	* @return a value between -1 and 1 indicating gas/brake pedal position
+	*/
+	flt getThrottleBrakePosition(flt curSpeed, flt wantedSpeed);
+    flt controlThrottleBrake(flt curSpeed, flt wantedSpeed);
+
+	/** calculate a steering position
+	* @return a value between -1 and 1 indicating steering position
+	*/
+    flt getSteeringPosition(aa::modules::nav::controller::Plan_ptr plan);
+    flt controlSteering(aa::modules::nav::controller::Plan_ptr plan);
+
+
+
+};
+
+}
+}
+}
+}
