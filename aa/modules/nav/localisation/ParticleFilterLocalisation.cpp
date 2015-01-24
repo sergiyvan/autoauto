945fc67162a5ca9e358895fe968edc6b3610e66a
chrise <christian_damm@hotmail.de>
2015-01-24 12:07:02 +0100

    Teillloesung

--- aa/modules/nav/localisation/ParticleFilterLocalisation.cpp
+++ aa/modules/nav/localisation/ParticleFilterLocalisation.cpp
@@ -137,19 +137,66 @@ void ParticleFilterLocalisation::stopHook()
 }
 
 void ParticleFilterLocalisation::predictionStep()
-{
+{    
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0,0.1);
+    double x_norm = 0.0;
+    double y_norm = 0.0;
+    double theta_norm = 0.0;
+
     //generate new particles
     // TODO...
+    int i =0;
+    while(mParticles.size() < 100){
+        i = rand() % mParticles.size();
+        math::flt randx = mParticles[i][0];
+        math::flt randy = mParticles[i][1];
+        math::flt randtheta = mParticles[i][2];
+        mParticles.push_back(math::Vec3(randx, randy, randtheta));
+
+    }
+
 
     //predict particle movement
     //TODO ...
+     for (int j=0;j<mParticles.size(); j++) {
+         x_norm = distribution(generator);
+         y_norm = distribution(generator);
+         theta_norm = distribution(generator);
+         mParticles[j] = mParticles[j] +mOdometryOnly + (math::Vec3(x_norm,y_norm,theta_norm));
+     }
 }
 
 void ParticleFilterLocalisation::updateStep()
 {
+
+    float dist_l_z;
+    float dist_l_x;
+    float theta_l_z;
+    float theta_l_x;
+
     //update weights
     //TODO ...
+   // float obst_1_x = mMeasurements[0][0];
+   //float obst_1_y = mMeasurements[0][1];
+   // float obst_2_x = mMeasurements[1][0];
+   // float obst_2_y = mMeasurements[0][1];
 
+   // std::cout << "!!!!!!!!!!!!!!!!!!!" << std::endl;
+   // std::cout << "mMeasurements: "<< mMeasurements[0][0]<< ";" << mMeasurements[0][1] << std::endl;
+
+    //loop over all weights
+    for (int i=0;i<mParticleWeights.size(); i++) {
+        //distanz = sqrt(x²+y²)
+        mParticleWeights[i]= 1.; //vllt falsch
+
+        //loop over all obstacles
+        for(int j =0; j<mMap.size(); j++){
+            dist_l_z = sqrt( (mMeasurements[j][0]^2) + (mMeasurements[j][1])^2);
+            dist_l_x = sqrt( ((abs(mParticles[i][0]-mMap[j][0]))^2)  + ((abs(mParticles[i][1]-mMap[j][1]))^2));
+        mParticleWeights[i] *=  exp(-((dist_l_z-dist_l_x)^2));
+        }
+    }
 
     //normalize weights
     //TODO ...
