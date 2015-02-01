dbd282b5963fc21b9f9e4c5a3eab5b50c361f6c6
chrise <christian_damm@hotmail.de>
2015-02-01 15:32:59 +0100

    Loesung zur Aufgabe

--- aa/modules/vision/ImageMatching.cpp
+++ aa/modules/vision/ImageMatching.cpp
@@ -9,6 +9,7 @@
 #include <util/TaskContextFactory.h>
 #include <util/OrocosHelperFunctions.h>
 #include <math/Types.h>
+#include <math.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/nonfree/features2d.hpp>
@@ -87,9 +88,82 @@ bool ImageMatching::startHook()
     cv::vector<cv::DMatch> matches;
     matcher.match(descriptors1,descriptors2,matches);
 
-    //draw matches
+
+
+    ///TODO: implement RANSAC
+    ///
+    math::flt max;
+    int index;
+    cv::DMatch match;
+    cv::DMatch temp_match;
+    cv::DMatch best_match;
+    std::vector<cv::DMatch> maybe_inliner;
+    std::vector<cv::DMatch> better_inliner;
+    std::vector<cv::DMatch> best_inliner;
+    float offset;
+    float best_offset;
+    float temp_offset;
+    int cur_num_inliner = 0;
+    int best_num_inliner = 0;
+    float e = 0.7;
+    float p = 0.99;
+    int N;
+
+    do{
+        N =(int) std::log((1. - p))  / std::log(1.-(1.-e));
+        best_inliner.clear();
+        best_inliner.insert(best_inliner.end(),better_inliner.begin(),better_inliner.end());
+        better_inliner.clear();
+
+        best_num_inliner = cur_num_inliner;
+        int j = 0;
+        while(j < N){
+            //get new random matches
+            max = matches.size()-1;
+            index = (int)(rand() * (max) / RAND_MAX);
+            //index = (int)(rand() * (max-min) / RAND_MAX)+min;
+            match = matches[index];
+
+           //get offset (only x direction)
+           offset = abs(keypoints1[match.queryIdx].pt.x - keypoints2[match.trainIdx].pt.x);
+
+           //loop over all matches
+           for(int i=0; i<matches.size();i++){
+               temp_match = matches[i];
+               temp_offset= abs(keypoints1[temp_match.queryIdx].pt.x-keypoints2[temp_match.trainIdx].pt.x);
+
+               //offset difference is smalles as one pixel
+               if(abs(offset-temp_offset)< 1. &&abs(offset-temp_offset)> -1.){
+                   maybe_inliner.push_back(temp_match);
+               }
+           }
+           //more inliner -> better offset
+           if(maybe_inliner.size()> better_inliner.size()){
+               better_inliner.clear();
+               better_inliner.insert(better_inliner.end(),maybe_inliner.begin(),maybe_inliner.end());
+               best_match = match;
+               best_offset = offset;
+           }
+           maybe_inliner.clear();
+           j++;
+        }
+        std::cout << " " << std::endl;
+        std::cout << "==========" << std::endl;
+        std::cout << "Gewaehltes e: " << e << std::endl;
+        std::cout << "Berechnetes N: " << N << std::endl;
+        std::cout << "Ermitterlter Offset: " << best_offset << std::endl;
+        std::cout << "Anzahl der Inlier: " << better_inliner.size() << std::endl;
+        std::cout << "=========="  << std::endl;
+        e = e-0.05;
+        cur_num_inliner = better_inliner.size();
+    }
+    while(cur_num_inliner >= best_num_inliner);
+
+    //draw matches -> draw image3
     cv::Mat output3;
-    cv::drawMatches(input1,keypoints1,input2,keypoints2,matches,output3,cv::Scalar::all(-1),cv::Scalar::all(-1), cv::vector<char>(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    cv::drawMatches(input1,keypoints1,input2,keypoints2,best_inliner,output3,cv::Scalar::all(-1),cv::Scalar::all(-1), cv::vector<char>(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+    //cv::drawMatches(input1,keypoints1,input2,keypoints2,matches,output3,cv::Scalar::all(-1),cv::Scalar::all(-1), cv::vector<char>(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
     //initialize images for display modules
     TimeStamp now;
@@ -102,26 +176,6 @@ bool ImageMatching::startHook()
     output2.copyTo(*mImage2);
     output3.copyTo(*mMatchedImage);
 
-
-    ///TODO: implement RANSAC
-
-    ///sample code for accessing matches/keypoints/coordinates
-    //draw random match
-    math::flt min = 0;
-    math::flt max = matches.size()-1;
-    math::flt randomvalue = (rand() * (max-min) / RAND_MAX)+min;
-    int index = (int)randomvalue;
-    cv::DMatch match = matches[index];
-
-    //get corresponding keypoint in first image
-    cv::KeyPoint kp1 = keypoints1[match.queryIdx];
-    //get corresponding keypoint in second image
-    cv::KeyPoint kp2 = keypoints2[match.trainIdx];
-
-    //get x coordinate
-    float kp1_x_coordinate = kp1.pt.x;
-
-    ///end sample code
 
 
     return true;
